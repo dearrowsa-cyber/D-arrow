c5c336f9343bcce00b14252dc62be5c92c36be74
Omar Abdelfattah <jklk9579@gmail.com>
2026-05-30 15:20:44 +0300

    Add Docker deployment files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   
   // Production optimizations
   productionBrowserSourceMaps: false,
